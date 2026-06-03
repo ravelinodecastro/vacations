8c97089368c14f7194cc638e4dde6317d9301c86
Ravelino Castro <ravelinodecastro@gmail.com>
2026-06-03 16:38:42 +0100

    add swagger config

---
+++ back-end/src/main/java/com/lbc/vacations/configs/OpenApiConfig.java
@@ -0,0 +1,63 @@
+package com.lbc.vacations.configs;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.License;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class OpenApiConfig {
+
+    private static final String SECURITY_SCHEME_NAME = "bearerAuth";
+
+    @Bean
+    public OpenAPI openAPI() {
+
+        return new OpenAPI()
+
+                .info(
+                        new Info()
+                                .title("Vacation Management API")
+                                .description("""
+                                        API for employee and vacation request management.
+
+                                        Roles:
+                                        - ADMIN
+                                        - MANAGER
+                                        - COLLABORATOR
+                                        """)
+                                .version("1.0.0")
+                                .contact(
+                                        new Contact()
+                                                .name("TaskFlow")
+                                                .email("support@taskflow.com")
+                                )
+                                .license(
+                                        new License()
+                                                .name("MIT")
+                                )
+                )
+
+                .addSecurityItem(
+                        new SecurityRequirement()
+                                .addList(SECURITY_SCHEME_NAME)
+                )
+
+                .components(
+                        new Components()
+                                .addSecuritySchemes(
+                                        SECURITY_SCHEME_NAME,
+                                        new SecurityScheme()
+                                                .name(SECURITY_SCHEME_NAME)
+                                                .type(SecurityScheme.Type.HTTP)
+                                                .scheme("bearer")
+                                                .bearerFormat("JWT")
+                                )
+                );
+    }
+}
