18989c6b8d920f2d10695181390a14b6bb5468d8
Ravelino Castro <ravelinodecastro@gmail.com>
2026-06-03 15:21:45 +0100

    define domain of employee

---
+++ back-end/src/main/java/com/lbc/vacations/employee/domain/Employee.java
@@ -0,0 +1,33 @@
+package com.lbc.vacations.employee.domain;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private UUID id;
+
+    @Column(nullable = false)
+    private String sub;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "manager_id")
+    private Employee manager;
+}
