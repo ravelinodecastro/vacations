68e4d1a33606613560c4e2f4ea2a92899aed8e2b
Ravelino Castro <ravelinodecastro@gmail.com>
2026-06-03 14:11:34 +0100

    first commit

---
+++ back-end/src/test/java/com/lbc/vacations/VacationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lbc.vacations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VacationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
